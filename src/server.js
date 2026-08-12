27461868c3732b09cfebcce369dcfa7e99c09dac
JucaHF24 <juancarlos,fr6@gmail.com>
2026-08-12 02:07:50 -0600

    Ajuste

--- src/server.js
+++ src/server.js
@@ -1,7 +1,8 @@
 const app = require('./app');
 
-const PORT = 3000;
+// Utilizar el puerto que asigna Render (process.env.PORT) o 3000 en desarrollo local
+const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, '0.0.0.0', () => {
-    console.log(`Servidor ejecutándose en http://localhost:${PORT}`);
+    console.log(`Servidor ejecutándose en el puerto ${PORT}`);
 });
